755b9d2212b6ffb17c88e2b79fd02a7125d94b37
James Skemp <strivinglife@gmail.com>
2019-09-11 10:43:20 -0500

    Additional attempts to run TypeScript tests

--- scripts/src/tests.ts
+++ scripts/src/tests.ts
@@ -1,8 +1,17 @@
-import { DiceRoller, DiceRoll } from "../../types/index"
-
-let diceRoller = new DiceRoller();
+// WIP. There are two code blocks here, which can be (un)commented as needed to test two different ways of referencing the TS Defs file.
+/*
+/// <reference path="../../types/index.d.ts" />
+let diceRoller: rpgDiceRoller.DiceRoller = new rpgDiceRoller.DiceRoller();
 const roll = diceRoller.roll("3d6");
 console.log(roll);
 
-let diceRoll = new DiceRoll("3d6");
+let diceRoll: rpgDiceRoller.DiceRoll = new rpgDiceRoller.DiceRoll("3d6");
+console.log(diceRoll.roll());
+*/
+import { DiceRoller, DiceRoll } from "../../types/index"
+let diceRoller: rpgDiceRoller.DiceRoller = new DiceRoller();
+const roll = diceRoller.roll("3d6");
+console.log(roll);
+
+let diceRoll: rpgDiceRoller.DiceRoll = new DiceRoll("3d6");
 console.log(diceRoll.roll());
